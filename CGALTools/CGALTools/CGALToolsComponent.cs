547be95f03a6c271592e080f893ca3f06c789290
Qiang <zhan_qiang@yeah.net>
2023-05-30 22:57:08 +0800

    update

--- CGALTools/CGALTools/CGALToolsComponent.cs
+++ CGALTools/CGALTools/CGALToolsComponent.cs
@@ -1,25 +1,14 @@
-using CGALDotNet.Polygons;
-using CGALDotNet;
-using Grasshopper;
 using Grasshopper.Kernel;
 using Grasshopper.Kernel.Types;
 using Rhino.Geometry;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
 
 namespace CGALTools
 {
     public class CGALToolsComponent : GH_Component
     {
-        /// <summary>
-        /// Each implementation of GH_Component must provide a public 
-        /// constructor without any arguments.
-        /// Category represents the Tab in which the component will appear, 
-        /// Subcategory the panel. If you use non-existing tab or panel names, 
-        /// new tabs/panels will automatically be created.
-        /// </summary>
         public CGALToolsComponent()
           : base("CGALOffset", "CGALOffset",
             "Offset Tool",
@@ -27,55 +16,24 @@ namespace CGALTools
         {
         }
 
-        /// <summary>
-        /// Registers all the input parameters for this component.
-        /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
-            // Use the pManager object to register your input parameters.
-            // You can often supply default values when creating parameters.
-            // All parameters must have the correct access type. If you want 
-            // to import lists or trees of values, modify the ParamAccess flag.
-
             pManager.AddCurveParameter("Polygon", "P", "Offset Polygon", GH_ParamAccess.item);
             pManager.AddNumberParameter("Distance", "D", "Amount to offset", GH_ParamAccess.item, 1);
-
-            // If you want to change properties of certain parameters, 
-            // you can use the pManager instance to access them by index:
-            //pManager[0].Optional = true;
         }
 
-        /// <summary>
-        /// Registers all the output parameters for this component.
-        /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            // Use the pManager object to register your output parameters.
-            // Output parameters do not have default values, but they too must have the correct access type.
             pManager.AddCurveParameter("OffsetResult", "R", "Offset Result", GH_ParamAccess.list);
-            // Sometimes you want to hide a specific parameter from the Rhino preview.
-            // You can use the HideParameter() method as a quick way:
-            //pManager.HideParameter(0);
         }
 
-        /// <summary>
-        /// This is the method that actually does the work.
-        /// </summary>
-        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
-        /// to store data in output parameters.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
-            // First, we need to retrieve all data from the input parameters.
-            // We'll start by declaring variables and assigning them starting values.
-
             Curve polygonCrv = null;
             double distance = 1;
 
-            // Then we need to access the input parameters individually. 
-            // When data cannot be extracted from a parameter, we should abort this method.
             if (!DA.GetData(0, ref polygonCrv)) return;
             if (!DA.GetData(1, ref distance)) return;
-
 
             if (!polygonCrv.IsClosed) return;
             if (!polygonCrv.IsPolyline()) return;
@@ -95,7 +53,7 @@ namespace CGALTools
                 points = points.Reverse().ToArray();
             }
             Array.Resize(ref points, points.Length - 1);
-            
+
             //calculator the offset result
             var interiorList = PolygonOffset.OffsetPolygon(GeometryConversion.RhinoPoint3ds2CGALPoint2ds(points), distance);
             List<GH_Curve> result = new List<GH_Curve>();
@@ -107,14 +65,14 @@ namespace CGALTools
                 var resPolygon = new PolylineCurve(pointArray);
 
                 //transform back
-                if(!resPolygon.Transform(Transform.PlaneToPlane(Plane.WorldXY, plane0))) return;
+                if (!resPolygon.Transform(Transform.PlaneToPlane(Plane.WorldXY, plane0))) return;
                 result.Add(new GH_Curve(resPolygon));
             }
-            
 
             // Finally assign the spiral to the output parameter.
             DA.SetDataList(0, result);
         }
+
         private bool IsClockwise(Point3d[] polygon)
         {
             double signedArea = 0.0;
@@ -129,11 +87,10 @@ namespace CGALTools
             return signedArea >= 0;
         }
 
-
         /// <summary>
-        /// The Exposure property controls where in the panel a component icon 
-        /// will appear. There are seven possible locations (primary to septenary), 
-        /// each of which can be combined with the GH_Exposure.obscure flag, which 
+        /// The Exposure property controls where in the panel a component icon
+        /// will appear. There are seven possible locations (primary to septenary),
+        /// each of which can be combined with the GH_Exposure.obscure flag, which
         /// ensures the component will only be visible on panel dropdowns.
         /// </summary>
         public override GH_Exposure Exposure => GH_Exposure.primary;
@@ -146,13 +103,12 @@ namespace CGALTools
         /// </summary>
         protected override System.Drawing.Bitmap Icon
         {
-            get{ return Properties.Resources.icon; }
+            get { return Properties.Resources.icon; }
         }
 
-
         /// <summary>
-        /// Each component must have a unique Guid to identify it. 
-        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// Each component must have a unique Guid to identify it.
+        /// It is vital this Guid doesn't change otherwise old ghx files
         /// that use the old ID will partially fail during loading.
         /// </summary>
         public override Guid ComponentGuid => new Guid("810a89a8-c839-49f3-82ac-d9701c007049");
